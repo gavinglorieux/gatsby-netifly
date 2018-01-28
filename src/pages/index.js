9593721802531e2c887c2ba9e0dc76e6f940c32f
Gavin Glorieux <gavin@joinblink.com>
2018-01-28 23:29:27 +0000

    markdown pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(({ node }) => <a key={node.id} href={`${node.frontmatter.path}`}>{node.frontmatter.title}</a>);
+
+  return <div>{Posts}</div>;
+};
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
